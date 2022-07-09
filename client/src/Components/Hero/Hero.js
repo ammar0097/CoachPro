d05d88978b4745b0b7a2003fb749dc805e1e2ad6
Ammar <ammarferchichi7@gmail.com>
2022-07-09 09:57:08 +0100

    project initialization

---
+++ client/src/Components/Hero/Hero.js
@@ -0,0 +1,69 @@
+import React from "react";
+import {
+  Box,
+  Heading,
+  Container,
+  Text,
+  Button,
+  Stack,
+  Image,
+  Center,
+} from "@chakra-ui/react";
+import "./Hero.css";
+import plLogo from "./premier-league-logo.png";
+
+function Hero() {
+  return (
+    <>
+      <Container maxW={"3xl"}>
+        <Stack
+          as={Box}
+          textAlign={"center"}
+          spacing={{ base: 8, md: 14 }}
+          py={{ base: 20, md: 36 }}
+        >
+          <Center>
+            <Image src={plLogo} alt="premier league logo" boxSize="150px" />
+          </Center>
+
+          <Heading
+            fontWeight={600}
+            fontSize={{ base: "2xl", sm: "4xl", md: "6xl" }}
+            lineHeight={"110%"}
+          >
+            Manage your team with <br />
+            <Text as={"span"} color={"#37003c"}>
+              CoachPro
+            </Text>
+          </Heading>
+          <Text color={"white"}>
+            Monetize your content by charging your most loyal readers and reward
+            them loyalty points. Give back to your loyal readers by granting
+            them access to your pre-releases and sneak-peaks.
+          </Text>
+          <Stack
+            direction={"column"}
+            spacing={3}
+            align={"center"}
+            alignSelf={"center"}
+            position={"relative"}
+          >
+            <Button
+              colorScheme={"green"}
+              bg={"#37003c"}
+              rounded={"full"}
+              px={6}
+              _hover={{
+                bg: "green.500",
+              }}
+            >
+              Get Started
+            </Button>
+          </Stack>
+        </Stack>
+      </Container>
+    </>
+  );
+}
+
+export default Hero;
